4abb349c2cb033c40406a928a33d489ecdad8975
Gsus4 <mikechen90091@gmail.com>
2018-07-11 12:17:16 +0800

    update something

--- Week_2/Data_Visualization.R
+++ Week_2/Data_Visualization.R
@@ -1,20 +1,11 @@
 library("ggplot2")
+#中西部人口統計數據
 midwest
-
-ggplot(data = midwest, aes(x = category)) +
-  geom_bar(fill = "steelblue", colour = "black")
-
-ggplot(data = midwest, aes(x = popdensity)) +
-  geom_histogram(binwidth = 500, fill = "steelblue")
-
-ggplot(data = midwest, aes(x = category)) +
-  geom_bar(fill = "steelblue", colour = "black")
-
-ggplot(data = midwest, aes(x = popdensity)) +
-  geom_histogram(binwidth = 500, fill = "steelblue")
-
-ggplot(data = midwest, aes(x = popdensity, y=poppovertyknown)) +
-  geom_point()
-
-ggplot(midwest, aes(x=category, y=poppovertyknown)) +
-  geom_boxplot()
+#單變數：類別型
+ggplot(data = midwest, aes(x = category)) + geom_bar(fill = "steelblue", colour = "black")
+#單變數：連續型
+ggplot(data = midwest, aes(x = popdensity)) + geom_histogram(binwidth = 500, fill = "steelblue")
+#連續 vs 連續
+ggplot(data = midwest, aes(x = popdensity, y=poppovertyknown)) + geom_point()
+#雙變數：離散 vs 連續
+ggplot(midwest, aes(x=category, y=poppovertyknown)) + geom_boxplot()
